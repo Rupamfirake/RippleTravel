d61c981cd61b8bdb50a3419e2f89247bc29e19f1
Rupam Firake <82810436+Rupamfirake@users.noreply.github.com>
2023-08-17 23:16:28 +0530

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,40 @@
+'use strict';
+
+/**
+ * navbar toggle
+ */
+
+const navToggleBtn = document.querySelector("[data-nav-toggle-btn]");
+const header = document.querySelector("[data-header]");
+
+navToggleBtn.addEventListener("click", function () {
+  this.classList.toggle("active");
+  header.classList.toggle("active");
+});
+
+
+
+/**
+ * Smooth Scrolling Anchor Links to Jump to specific part of a Web Page
+ */
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+      });
+  });
+});
+
+
+/**
+ * show go top btn when scroll window to 500px
+ */
+
+const goTopBtn = document.querySelector("[data-go-top]");
+
+window.addEventListener("scroll", function () {
+  window.scrollY >= 500 ? goTopBtn.classList.add("active")
+    : goTopBtn.classList.remove("active");
+});
